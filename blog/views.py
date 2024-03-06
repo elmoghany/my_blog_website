2f712319f5634d0ac1e1035a5587e0cbc0db54a3
Moghany <m.osama.elmoghany@gmail.com>
2024-03-06 11:07:26 +0200

    fixed bug in post details page

--- blog/views.py
+++ blog/views.py
@@ -2,12 +2,13 @@
 from typing import Any
 from django.db.models.query import QuerySet
 from django.shortcuts import render, get_object_or_404
-from .models import Post
-from .forms import CommentForm
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views.generic import ListView, DetailView
 from django.views import View
+
+from .models import Post
+from .forms import CommentForm
 # all_posts =[
 #     {
 #         "slug": "hike-in-the-mountains",
@@ -92,7 +93,7 @@ class StartingPageView(ListView):
         return data
 
 class AllPostsView(ListView):
-    template_name = "blog/all-posts.htmls"
+    template_name = "blog/all-posts.html"
     model = Post
     ordering = "-date"
     context_object_name = "all_posts"
@@ -105,7 +106,7 @@ class SinglePostView(View):
     def get(self, request, slug):
         post  = Post.objects.get(slug=slug)
         context={
-            "post":"post",
+            "post":post,
             "post_tags": post.tags.all(),
             "comment_form": CommentForm(),
             "comments": post.comments.all().order_by("-id")
@@ -122,7 +123,7 @@ class SinglePostView(View):
             return HttpResponseRedirect(reverse("post-detail-page", 
                                                 args=[slug]))
         context={
-            "post":"post",
+            "post":post,
             "post_tags": post.tags.all(),
             "comment_form": CommentForm(),
             "comments": post.comments.all().order_by("-id")
