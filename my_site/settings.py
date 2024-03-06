df454afd2bbd21a235221a5e1d8933e79d37f835
Moghany <m.osama.elmoghany@gmail.com>
2024-03-06 15:44:17 +0200

    static-files config

--- my_site/settings.py
+++ my_site/settings.py
@@ -81,10 +81,20 @@ WSGI_APPLICATION = 'my_site.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'posgres',
+        'USER': 'djangoblog',
+        'PASSWORD': '123456789',
+        'HOST': 'endpoint',
+        'PORT': '5432'
     }
 }
 
